3b547dde23a357b6311a93a205502fa09379cf18
Jason <quan.v.tran-1@ou.edu>
2025-09-11 15:29:58 -0500

    HW1 Submission

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,43 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            double num1;
+            double num2;
+            double subtotal;
+            double salestax;
+            double total;
+
+            const double taxrate = 0.085;
+
+            string num1AsString;
+            string num2AsString;
+
+
+
+            Console.WriteLine("What is the product name of the item you are purchasing ?");
+                 String ItemName = Console.ReadLine();
+            Console.WriteLine("How many " + ItemName + " do you want to buy?");
+                 num1AsString = Console.ReadLine();
+            Console.WriteLine("What is the price for each " + ItemName + " ?");
+                num2AsString = Console.ReadLine();
+
+            num1 = Convert.ToDouble(num1AsString);
+            num2 = Convert.ToDouble(num2AsString);
+
+            subtotal = num1 * num2;
+            salestax = subtotal * taxrate;
+            total = subtotal + salestax;
+
+
+            Console.WriteLine("Your subtotal for your bill is " + subtotal.ToString("C2"));
+            Console.WriteLine("Your sales tax for your bill is " + salestax.ToString("C2"));
+            Console.WriteLine("Your total for your bill is " + total.ToString("C2"));
+
+
+                
+
+
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,7 +1,7 @@
 ﻿// HW1b Grade
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Jason Tran 
+// Did you seek help ? If yes, specify the helper or web link here: chatgpt, reason: to find the formula to calculate the final grade based on the weight  
 
 using System;
 using System.Collections.Generic;
@@ -15,6 +15,59 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            double num1;
+            double num2;
+            double num3;
+            double num4;
+            double num5;
+            double FinalGrade;
+
+            string assignments;
+            string exam1;
+            string exam2;
+            string exam3;
+            string participations;
+
+
+            Console.WriteLine("What is your first name?");
+            string FName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string LName = Console.ReadLine();
+            Console.WriteLine("What is your student id?");
+            string id = Console.ReadLine();
+
+
+            Console.WriteLine("What is your overall percentage grade for assignments?");
+            assignments = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for Exam 1?");
+            exam1 = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for Exam 2?");
+            exam2 = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for Exam 3?");
+            exam3 = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            participations = Console.ReadLine();
+
+
+            num1 = Convert.ToDouble(assignments);
+            num2 = Convert.ToDouble(exam1);
+            num3 = Convert.ToDouble(exam2);
+            num4 = Convert.ToDouble(exam3);
+            num5 = Convert.ToDouble(participations);
+
+            FinalGrade = ((num1 * .2) + (num2 * .15) + (num3 * .25) + (num4 * .25) + (num5 * .15)) / 100;
+
+
+            Console.WriteLine(FName + (" ") + LName + " (" + id + "), " + "your final grade is " + FinalGrade.ToString("P2"));
+
+
+
+
+
+
+
+
+
         }
     }
 }
